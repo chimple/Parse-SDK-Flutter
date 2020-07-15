6518a186d03d99192a9d6fa747556b38b09966c9
Bhanu <62737055+bhanugummana@users.noreply.github.com>
2020-07-15 19:48:09 +0530

    Update parse_live_query.dart

--- lib/src/network/parse_live_query.dart
+++ lib/src/network/parse_live_query.dart
@@ -48,7 +48,6 @@ class LiveQueryReconnectingController with WidgetsBindingObserver {
       this._reconnect, this._eventStream, this.debug) {
     // Connectivity().checkConnectivity().then(_connectivityChanged);
     // Connectivity().onConnectivityChanged.listen(_connectivityChanged);
-    _setReconnect();
     _eventStream.listen((LiveQueryClientEvent event) {
       switch (event) {
         case LiveQueryClientEvent.CONNECTED:
